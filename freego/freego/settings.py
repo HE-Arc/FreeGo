d42d4da5a09487547a7f0480af86cfa2965b674e
gael.christe <gael.christe@he-arc.ch>
2020-02-27 15:39:31 +0100

    correct template problem

--- freego/freego/settings.py
+++ freego/freego/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fridge',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'freego.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- freego/fridge/views.py
+++ freego/fridge/views.py
@@ -4,6 +4,5 @@ from django.views import generic, View
 from django.urls import reverse_lazy
 
 def index(request):
-    
     context = {}
     return render(request, 'fridge/index.html', context)
