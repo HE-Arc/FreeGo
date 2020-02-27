b528717f8c20b144fa021660918ea048b79f25b1
gael.christe <gael.christe@he-arc.ch>
2020-02-27 12:53:24 +0100

    create Fridge model

--- freego/fridge/models.py
+++ freego/fridge/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Fridge(models.Model):
+    name = models.CharField(max_length=200)
+    adresse = models.CharField(max_length=200)
+    NPA = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    user_id = models.IntegerField() #FIXME faire référence seulement à l'ID ou a toute la table
