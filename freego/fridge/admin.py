7d46b0b0ffdf94c648204239e36e15ad6990eb90
gael.christe <gael.christe@he-arc.ch>
2020-02-27 14:11:07 +0100

    create models and migrate db

--- freego/fridge/admin.py
+++ freego/fridge/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Fridge, User, Food, Reporting
+
+admin.site.register(Fridge)
+admin.site.register(User)
+admin.site.register(Food)
+admin.site.register(Reporting)
