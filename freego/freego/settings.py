f86952748053392241fc200489505ffebe252dc9
gaelchriste <43579050+gaelchriste@users.noreply.github.com>
2020-03-04 16:36:48 +0100

    Database (#23)
    * create models and migrate db
    
    * update model food

--- freego/freego/settings.py
+++ freego/freego/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fridge.apps.FridgeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- freego/fridge/models.py
+++ freego/fridge/models.py
@@ -1,8 +1,54 @@
 from django.db import models
 
+
 class Fridge(models.Model):
-    name = models.CharField(max_length=200)
-    adresse = models.CharField(max_length=200)
-    NPA = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=200)
-    user_id = models.IntegerField() #FIXME faire référence seulement à l'ID ou a toute la table
+    '''Fridge model'''
+    name = models.CharField(max_length=45)
+    adresse = models.CharField(max_length=45)
+    NPA = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=12)
+    user = models.ForeignKey(
+        'User', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class User(models.Model):
+    '''User model'''
+    name = models.CharField(max_length=45)
+    first_name = models.CharField(max_length=255)
+    status = models.IntegerField()
+    email = models.CharField(max_length=45)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Food(models.Model):
+    '''Food model'''
+    name = models.CharField(max_length=45)
+    vegetarian = models.BooleanField()
+    vegan = models.BooleanField()
+    expiration_date = models.DateField()
+    fridge = models.ForeignKey(
+        'Fridge', on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(
+        'User', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Reporting(models.Model):
+    '''Reporting model'''
+    title = models.CharField(max_length=45)
+    description = models.CharField(max_length=500)
+    fridge = models.ForeignKey(
+        'Fridge', on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(
+        'User', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.title)
