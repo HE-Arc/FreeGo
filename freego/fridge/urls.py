e090b6496d3a4b6daea0b872c1501c6c6a506387
gael.christe <gael.christe@he-arc.ch>
2020-03-07 15:57:11 +0100

    add urls and views for fridge model

--- freego/fridge/urls.py
+++ freego/fridge/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'fridge'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('fridges', views.FridgeListView.as_view(), name="fridges"),
+    path('fridge/detail/<pk>', views.FridgeDetailView.as_view(), name="fridge-detail"),
+    path('fridge/new', views.FridgeCreateView.as_view(), name="fridge-form"),
+    path('fridge/<pk>/update', views.FridgeUpdateView.as_view(), name="fride-update"),
+    path('fridge/<pk>/delete', views.FridgeDeleteView.as_view(), name="fridge-delete")
 ]
--- freego/fridge/views.py
+++ freego/fridge/views.py
@@ -1,5 +1,25 @@
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views import generic, View
+from django.urls import reverse_lazy
 
+from .models import Fridge, Food
 
 def index(request):
     return HttpResponse("Hello, world. You're at the fridge index.")
+
+class FridgeListView(generic.ListView):
+    model = Fridge
+
+class FridgeDetailView(generic.DetailView):
+    model = Fridge
+
+class FridgeCreateView(generic.CreateView):
+    model = Fridge
+    fields = ['name', 'address', 'NPA', 'phone_number', 'user']
+
+class FridgeUpdateView(generic.UpdateView):
+    model = Fridge
+
+class FridgeDeleteView(generic.DeleteView):
+    model = Fridge
