a4bab3f7ac7541f0ceaf9fff00b65f55fc261a80
gael.christe <gael.christe@he-arc.ch>
2020-03-11 16:21:44 +0100

    test for github actions

--- freego/freego/settings.py
+++ freego/freego/settings.py
@@ -83,6 +83,14 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
     },
+    'github_actions': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'freego_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3800',
+    },
     'default_sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
