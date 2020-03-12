3d40fca4668124119518688366a2fdd9f2dd5730
gaelchriste <43579050+gaelchriste@users.noreply.github.com>
2020-03-12 14:58:21 +0100

    Database (#26)
    * create models and migrate db
    
    * update model food
    
    * add urls and views for fridge model
    
    * create crud for fridge
    
    * create crud food
    
    * update requirements
    
    * remake virtual env and requirements.txt file
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * test for github actions
    
    * Update pythonpackage.yml
    
    * test for github actions
    
    * test for github actions
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * test for github actions
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * test for github actions
    
    * test for github actions
    
    * Update pythonpackage.yml
    
    * test for github actions
    
    * test for github actions
    
    * test for github actions
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    Co-authored-by: gael.christe <gael.christe@he-arc.ch>

--- freego/freego/settings.py
+++ freego/freego/settings.py
@@ -25,13 +25,12 @@ SECRET_KEY = '$g+b++)$rxeitv#3h7+e0k%=a)4)w3*ba(a-8vu(jy*_77irgs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'fridge.apps.FridgeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,10 +75,26 @@ WSGI_APPLICATION = 'freego.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'freego_db',
+    #     'USER': 'root',
+    #     'PASSWORD': 'root',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    # },
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'freego_db2',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3800',
+    },
+    'default_sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
 }
 
 
@@ -120,4 +135,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
