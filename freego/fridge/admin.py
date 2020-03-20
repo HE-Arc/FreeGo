04c2432b90d51bc01d78ccdc35524e87450d4d26
gaelchriste <43579050+gaelchriste@users.noreply.github.com>
2020-03-20 11:01:08 +0100

    Schedule tables (#31)
    * update requirements.txt and settings.py
    
    * add opening_hours and special_days tables
    
    * update settings
    
    Co-authored-by: gael.christe <gael.christe@he-arc.ch>

--- freego/fridge/admin.py
+++ freego/fridge/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Fridge, User, Food, Reporting
+from .models import Fridge, User, Food, Reporting, OpeningHour, SpecialDay
 
 admin.site.register(Fridge)
 admin.site.register(User)
 admin.site.register(Food)
 admin.site.register(Reporting)
+admin.site.register(OpeningHour)
+admin.site.register(SpecialDay)
