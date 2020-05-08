e62ce6e64bfea3f43dbb73c444018cffbbad7c8a
gaelchriste <43579050+gaelchriste@users.noreply.github.com>
2020-05-08 22:55:30 +0200

    Sonar (#56)
    * add sonar scanner
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * add travis
    
    * update .travis.yml
    
    * remove pythonpackage
    
    * remove pythonpackage
    
    * update travis.yml
    
    * update .travis.yml
    
    * update .travis.yml
    
    * update .travis.yml
    
    * update .travis.yml
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    Co-authored-by: gaelchriste <gael.christe@he-arc.ch>

--- freego/settings/production_settings.py
+++ freego/settings/production_settings.py
@@ -2,7 +2,7 @@ from freego.settings.common_settings import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SET environment variable SECRET_KEY on server
-SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -27,17 +27,33 @@ SECURE_REFERRER_POLICY = 'no-referrer'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+#Use the following live settings to build on Travis CI
+if os.getenv('TRAVIS', None):
+    SECRET_KEY = "n&g)n(y(rm3&k9e^2_=+70twpknh^(z&la1v)&8)dokmmiv1%q"
+    DEBUG = False
+    TEMPLATE_DEBUG = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('GROUPNAME', 'freego_db'),
-        'USER': os.environ.get('GROUPNAME', 'root'),
-        'PASSWORD': os.environ.get('PASSWORD', 'root'),
-        'HOST': os.environ.get('MYSQL_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('MYSQL_PORT', '3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4'
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'myapp_test',
+            'USER': 'travis',
+            'PASSWORD': '',
+            'HOST': '127.0.0.1',
         }
     }
-}
+else:
+    SECRET_KEY = os.environ['SECRET_KEY']
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get('GROUPNAME', 'freego_db'),
+            'USER': os.environ.get('GROUPNAME', 'root'),
+            'PASSWORD': os.environ.get('PASSWORD', 'root'),
+            'HOST': os.environ.get('MYSQL_HOST', '127.0.0.1'),
+            'PORT': os.environ.get('MYSQL_PORT', '3306'),
+            'OPTIONS': {
+                'charset': 'utf8mb4'
+            }
+        }
+    }
