f78591751ccbb44718dfda30743938830f455bc6
gaelchriste <gael.christe@he-arc.ch>
2020-05-18 18:51:40 +0200

    fix errors

--- freego/settings/common_settings.py
+++ freego/settings/common_settings.py
@@ -13,10 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))))
-PWA_SERVICE_WORKER_PATH = os.path.join(
-    BASE_DIR, 'fridge/static/fridge/js', 'serviceworker.js')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'fridge/static/fridge/js', 'serviceworker.js')
 
 # insta_project/settings.py
 MEDIA_URL = '/media/'
@@ -116,7 +114,7 @@ TIME_INPUT_FORMATS = (
     '%I:%M %p',
 )
 
-APPEND_SLASH = False
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
--- fridge/urls.py
+++ fridge/urls.py
@@ -7,60 +7,60 @@ from . import views
 app_name = 'fridge'
 
 urlpatterns = [
-    path('fridge/list', views.FridgeListView.as_view(), name='fridge-list'),
+    path('fridge/list/', views.FridgeListView.as_view(), name='fridge-list'),
 
     # Admin
-    path('myadmin', views.AdminIndexView.as_view(), name='myadmin'),
+    path('myadmin/', views.AdminIndexView.as_view(), name='myadmin'),
 
     # Fridge
-    path('fridge/new', views.FridgeCreateView.as_view(), name='fridge-new'),
-    path('fridge/<pk>/delete', views.FridgeDeleteView.as_view(), name='fridge-delete'),
-    path('fridge/<pk>/update', views.FridgeUpdateView.as_view(), name='fridge-update'),
+    path('fridge/new/', views.FridgeCreateView.as_view(), name='fridge-new'),
+    path('fridge/<pk>/delete/', views.FridgeDeleteView.as_view(), name='fridge-delete'),
+    path('fridge/<pk>/update/', views.FridgeUpdateView.as_view(), name='fridge-update'),
 
     # Store
-    path('store', views.StoreIndexView.as_view(), name='store'),
-    path('store/detail', views.StoreDetailView.as_view(), name='store-detail'),
+    path('store/', views.StoreIndexView.as_view(), name='store'),
+    path('store/detail/', views.StoreDetailView.as_view(), name='store-detail'),
 
     # Food
-    path('food/new', views.FoodCreateView.as_view(),
+    path('food/new/', views.FoodCreateView.as_view(),
          name='food-form'),
-    path('food/<pk>/delete', views.FoodDeleteView.as_view(),
+    path('food/<pk>/delete/', views.FoodDeleteView.as_view(),
          name='food-delete'),
-    path('food/<pk>/list', views.FoodListView.as_view(), name='food-list'),
+    path('food/<pk>/list/', views.FoodListView.as_view(), name='food-list'),
 
     # Opening Day
-    path('openinghour/new', views.OpeningHourCreateView.as_view(),
+    path('openinghour/new/', views.OpeningHourCreateView.as_view(),
          name='openinghour-form'),
-    path('openinghour/<pk>/delete', views.OpeningHourDeleteView.as_view(),
+    path('openinghour/<pk>/delete/', views.OpeningHourDeleteView.as_view(),
          name='openinghour-delete'),
 
     # Special Day
-    path('specialday/new', views.SpecialDayCreateView.as_view(),
+    path('specialday/new/', views.SpecialDayCreateView.as_view(),
          name='specialday-form'),
-    path('specialday/<pk>/delete', views.SpecialDayDeleteView.as_view(),
+    path('specialday/<pk>/delete/', views.SpecialDayDeleteView.as_view(),
          name='specialday-delete'),
 
     # Reservation
-    path('food/<pk>/reservation', views.FoodReservation.as_view(),
+    path('food/<pk>/reservation/', views.FoodReservation.as_view(),
          name='food-reservation'),
-    path('food/<pk>/cancellation', views.FoodCancellation.as_view(),
+    path('food/<pk>/cancellation/', views.FoodCancellation.as_view(),
          name='food-cancellation'),
-    path('reservation/list', views.ReservationListView.as_view(),
+    path('reservation/list/', views.ReservationListView.as_view(),
          name='reservation-list'),
 
     # Settings
-    path('settings', views.SettingsView.as_view(), name='settings'),
+    path('settings/', views.SettingsView.as_view(), name='settings'),
 
     # Login/Register/Logout
-    path('register', views.RegisterView.as_view(), name='register'),
-    path('login', views.LoginView.as_view(), name='login'),
-    path('logout', views.LogoutView.as_view(), name='logout'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 
     # TODO (TB)
     path('', views.HomeView.as_view(), name='home'),
-    path('home', views.HomeView.as_view(), name='home'),
-    path('map', views.MapView.as_view(), name='map'),
-    path('favorite', views.FavoriteView.as_view(), name='favorite')
+    path('home/', views.HomeView.as_view(), name='home'),
+    path('map/', views.MapView.as_view(), name='map'),
+    path('favorite/', views.FavoriteView.as_view(), name='favorite')
 ]
 
 if settings.DEBUG:
