ed8058d94239dda1d63faa64804171d863daa4e0
gaelchriste <43579050+gaelchriste@users.noreply.github.com>
2020-05-20 17:02:37 +0200

    Pwa (#77)
    * first version of pwa
    
    * update pwa FILES_TO_CACHE and add icons
    
    * update pythonpackage.yml and move files
    
    * Update pythonpackage.yml
    
    * update requirements.txt
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * Update pythonpackage.yml
    
    * update yml
    
    * update yml
    
    * update xml
    
    * update yml
    
    * update yml
    
    * update ym
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * remove test.yml, doesn't work
    
    * pwa add offline page
    
    * update cdn for local js and css files
    
    * refactor
    
    * add test
    
    * remove unused files
    
    Co-authored-by: gaelchriste <gael.christe@he-arc.ch>

--- fridge/views.py
+++ fridge/views.py
@@ -149,9 +149,10 @@ class FoodDeleteView(LoginRequiredMixin, generic.DeleteView):
         return self.post(request, *args, **kwargs)
 
 
-class FoodListView(generic.ListView):
+class FoodListView(LoginRequiredMixin, generic.ListView):
     template_name = 'fridge/food_list.html'
     model = Food
+    login_url = LOGIN_URL
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -370,3 +371,12 @@ class MapView(generic.TemplateView):
 
 class FavoriteView(generic.TemplateView):
     template_name = 'fridge/favorite.html'
+
+######################################
+#                PWA                 #
+######################################
+
+
+def offline_view(request):
+    template = 'fridge/offline.html'
+    return render(request, template)
