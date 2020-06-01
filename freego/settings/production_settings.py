1e216e6fd4af88f084d2b3fc6e66bb0cece5d117
gael.christe <gael.christe@he-arc.ch>
2020-06-01 08:28:11 +0200

    update for deployement

--- freego/settings/production_settings.py
+++ freego/settings/production_settings.py
@@ -12,36 +12,17 @@ ALLOWED_HOSTS = ['0.0.0.0', 'localhost',
 
 SECURE_REFERRER_POLICY = 'no-referrer'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-# Use the following live settings to build on Travis CI
-if os.getenv('TRAVIS', None):
-    SECRET_KEY = "n&g)n(y(rm3&k9e^2_=+70twpknh^(z&la1v)&8)dokmmiv1%q"
-    DEBUG = False
-    TEMPLATE_DEBUG = True
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'myapp_test',
-            'USER': 'travis',
-            'PASSWORD': '',
-            'HOST': '127.0.0.1',
+SECRET_KEY = os.environ['SECRET_KEY']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('GROUPNAME', 'freego_db'),
+        'USER': os.environ.get('GROUPNAME', 'root'),
+        'PASSWORD': os.environ.get('PASSWORD', 'root'),
+        'HOST': os.environ.get('MYSQL_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4'
         }
     }
-else:
-    SECRET_KEY = os.environ['SECRET_KEY']
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ.get('GROUPNAME', 'freego_db'),
-            'USER': os.environ.get('GROUPNAME', 'root'),
-            'PASSWORD': os.environ.get('PASSWORD', 'root'),
-            'HOST': os.environ.get('MYSQL_HOST', '127.0.0.1'),
-            'PORT': os.environ.get('MYSQL_PORT', '3306'),
-            'OPTIONS': {
-                'charset': 'utf8mb4'
-            }
-        }
-    }
+}
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'freego.settings.local_settings')
+                          'freego.settings.production_settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
