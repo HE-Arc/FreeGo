0e341272e0ba2f6e61e5da202140513f02d580ce
gaelchriste <gael.christe@he-arc.ch>
2020-07-03 13:31:35 +0200

    update

---
+++ freego/settings/development_settings.py
@@ -0,0 +1,27 @@
+from freego.settings.common_settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# SET environment variable SECRET_KEY on server
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost',
+                 '127.0.0.1', 'freego.srvz-webapp.he-arc.ch']
+
+
+SECRET_KEY = os.environ['SECRET_KEY']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('GROUPNAME', 'freego_db'),
+        'USER': os.environ.get('GROUPNAME', 'root'),
+        'PASSWORD': os.environ.get('PASSWORD', 'root'),
+        'HOST': os.environ.get('MYSQL_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4'
+        }
+    }
+}
