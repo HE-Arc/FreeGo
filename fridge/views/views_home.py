dcc60e8a208dd0bc9683bcde8571d127103c4757
gaelchriste <gael.christe@he-arc.ch>
2020-07-03 13:00:28 +0200

    fix fridge access users vulnerability

--- fridge/views/views_home.py
+++ fridge/views/views_home.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from fridge.models import Fridge
 
 
 class HomeView(generic.TemplateView):
@@ -22,6 +23,8 @@ class SettingsView(generic.TemplateView):
 
         if self.request.user.is_authenticated:
             context['has_fridge'] = self.request.user.has_fridge()
+            context['fridge'] = Fridge.objects.filter(
+                user=self.request.user).first()
 
         return context
 
