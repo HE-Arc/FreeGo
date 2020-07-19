41eabfa01688d16acd3aab49cdba652caa78f8e5
gaelchriste <gael.christe@he-arc.ch>
2020-07-19 11:27:50 +0200

    update settings

--- freego/settings/production_settings.py
+++ freego/settings/production_settings.py
@@ -11,7 +11,6 @@ ALLOWED_HOSTS = ['0.0.0.0', 'localhost',
                  '127.0.0.1', 'freego.srvz-webapp.he-arc.ch']
 
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 # Use the following live settings to build on Travis CI
@@ -50,5 +49,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'freego.tb@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'test')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
