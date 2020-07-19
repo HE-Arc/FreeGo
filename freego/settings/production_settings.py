e7eda3749d4eaca0691810c671cd276ea05dc6c2
gaelchriste <gael.christe@he-arc.ch>
2020-07-19 08:38:21 +0200

    add real email

--- freego/settings/production_settings.py
+++ freego/settings/production_settings.py
@@ -44,3 +44,11 @@ else:
             }
         }
     }
+
+
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'freego.tb@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
