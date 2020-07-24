8230c97c4a21ccb667d5076d679370b11d9ccc7a
gael.christe <gael.christe@he-arc.ch>
2020-07-24 14:38:23 +0200

    add local_settings example

---
+++ freego/settings/local_settings.example.py
@@ -0,0 +1,25 @@
+from freego.settings.common_settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '<SECRET_KEY >'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+EMAIL_HOST = '<EMAIL_HOST >'
+EMAIL_HOST_USER = '<EMAIL_HOST_USER >'
+EMAIL_HOST_PASSWORD = '<EMAIL_HOST_PASSWORD >'
+EMAIL_PORT = '<EMAIL_PORT >'
