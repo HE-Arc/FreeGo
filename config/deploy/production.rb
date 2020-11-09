e6cd1d28cf7b2c25bfd0ccb14ac7bc0ca2bb95a4
gaelchriste <leag1@live.fr>
2020-11-09 20:09:53 +0100

    update capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "84.16.79.157", user: "debian", 
+server "84.16.79.157", user: "gael", 
 			                  roles: %w{app db web}
 
 
