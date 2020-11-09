363d34647645b52e523a7633e6de809c42099cb7
gaelchriste <leag1@live.fr>
2020-11-09 19:47:43 +0100

    update capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.12.1"
+lock "~> 3.14.1"
 
 set :application, "Free Go"
 set :repo_url, "https://github.com/HE-Arc/FreeGo.git"
