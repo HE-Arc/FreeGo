cd16f6b7d5ae7ab8fd9d3554d208b0ecfebe4a99
gaelchriste <leag1@live.fr>
2020-11-14 21:07:52 +0100

    update capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
-server "84.16.79.157", user: "gael", roles: %w{app db web}
+server "application-freego.ch", user: "gael", roles: %w{app db web}
 set :deploy_user, 'gael'
  
 set :branch, "production"
 set :stage, :production
  
-set :venv_path, "/home/gael/freego/freegoenv"
-SSHKit.config.command_map[:python] = "#{fetch :venv_path}/bin/python"
-SSHKit.config.command_map[:pip] = "#{fetch :venv_path}/bin/pip"
+set :venv_path, "/var/www/freego/freegoenv"
+SSHKit.config.command_map[:python] = "#{fetch :venv_path}/bin/python3.7"
+SSHKit.config.command_map[:pip] = "#{fetch :venv_path}/bin/pip3.7"
