cbd32a5cd3b621430840f1a6a22e3d0f56fd598f
gaelchriste <leag1@live.fr>
2020-11-15 12:24:27 +0100

    update email

--- freego/settings/production_settings.py
+++ freego/settings/production_settings.py
@@ -44,6 +44,6 @@ else:
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'infos.freego@gmail.com'
+EMAIL_HOST_USER = 'application.freego@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'test')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
